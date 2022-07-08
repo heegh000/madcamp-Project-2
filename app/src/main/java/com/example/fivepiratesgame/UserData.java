412bb156fc684086f8f89ccfbc1286790cdcb603
editadiary <blessedita@kaist.ac.kr>
2022-07-08 22:26:29 +0900

    avatar-hashmap

--- app/src/main/java/com/example/fivepiratesgame/UserData.java
+++ app/src/main/java/com/example/fivepiratesgame/UserData.java
@@ -36,19 +36,7 @@ public class UserData {
         this.userAvatar = AvatarID;
         this.userCash = cash;
     }
-    public int getAvatar(int AvatarID){
-        switch (AvatarID) {
-            case 0: return R.drawable.bulbasaur;
-            case 1: return R.drawable.charmander;
-            case 2: return R.drawable.egg;
-            case 3: return R.drawable.jigglypuff;
-            case 4: return R.drawable.meowth;
-            case 5: return R.drawable.pikachu;
-            case 6: return R.drawable.snorlex;
-            default: return R.drawable.squirtle;
-        }
 
-    }
 
 
 }
