74854ca177857c2ad7f65e9c09fd5748201f0443
editadiary <blessedita@kaist.ac.kr>
2022-07-12 00:55:01 +0900

    enter-empty-string-solved

--- app/src/main/java/com/example/fivepiratesgame/game/GameActivity.java
+++ app/src/main/java/com/example/fivepiratesgame/game/GameActivity.java
@@ -403,11 +403,22 @@ public class GameActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                int gold5 = Integer.parseInt(etG5.getText().toString());
-                int gold4 = Integer.parseInt(etG4.getText().toString());
-                int gold3 = Integer.parseInt(etG3.getText().toString());
-                int gold2 = Integer.parseInt(etG2.getText().toString());
-                int gold1 = Integer.parseInt(etG1.getText().toString());
+                int gold5, gold4, gold3, gold2, gold1;
+
+                if(etG5.getText().equals("")) { gold5 = 0; }
+                else gold5 = Integer.parseInt(etG5.getText().toString());
+
+                if(etG4.getText().equals("")) { gold4 = 0; }
+                else gold4 = Integer.parseInt(etG4.getText().toString());
+
+                if(etG3.getText().equals("")) { gold3 = 0; }
+                else gold3 = Integer.parseInt(etG3.getText().toString());
+
+                if(etG2.getText().equals("")) { gold2 = 0; }
+                else gold2 = Integer.parseInt(etG2.getText().toString());
+
+                if(etG1.getText().equals("")) { gold1 = 0; }
+                else gold1 = Integer.parseInt(etG1.getText().toString());
 
                 int total = gold5 + gold4 + gold3 + gold2 + gold1;
                 tvleftCoin.setText(String.valueOf(1000-total));
@@ -425,18 +436,22 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                int gold5 = Integer.parseInt(etG5.getText().toString());
-                int gold4 = Integer.parseInt(etG4.getText().toString());
-                int gold3 = Integer.parseInt(etG3.getText().toString());
-                int gold2 = Integer.parseInt(etG2.getText().toString());
-                int gold1 = Integer.parseInt(etG1.getText().toString());
+                int gold5, gold4, gold3, gold2, gold1;
 
                 if(etG5.getText().equals("")) { gold5 = 0; }
-                if(etG4.getText().equals("")) { gold4 = 0; }
-                if(etG3.getText().equals("")) { gold3 = 0; }
-                if(etG2.getText().equals("")) { gold2 = 0; }
-                if(etG1.getText().equals("")) { gold1 = 0; }
+                else gold5 = Integer.parseInt(etG5.getText().toString());
 
+                if(etG4.getText().equals("")) { gold4 = 0; }
+                else gold4 = Integer.parseInt(etG4.getText().toString());
+
+                if(etG3.getText().equals("")) { gold3 = 0; }
+                else gold3 = Integer.parseInt(etG3.getText().toString());
+
+                if(etG2.getText().equals("")) { gold2 = 0; }
+                else gold2 = Integer.parseInt(etG2.getText().toString());
+
+                if(etG1.getText().equals("")) { gold1 = 0; }
+                else gold1 = Integer.parseInt(etG1.getText().toString());
 
                 int total = gold5 + gold4 + gold3 + gold2 + gold1;
 
@@ -459,7 +474,6 @@ public class GameActivity extends AppCompatActivity {
                 }
             }
         });
-
 
         // Dialog 형태 지우기
         if(dialog.getWindow() != null){
